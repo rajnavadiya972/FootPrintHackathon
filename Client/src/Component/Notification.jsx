5c49ac0c55c49c2411199fc7834f6fa957e99971
rajnavadiya972 <rajnavadiya333@gmail.com>
2023-03-04 12:01:48 +0530

    Notification

---
+++ Client/src/Component/Notification.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+function Notification() {
+    return (
+        <div className='text-white'>
+            <div className='text-center'>Notification</div>
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+
+            <div className='flex justify-center my-2'>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border'/>
+                <div className='my-auto mx-3'>
+                    <div>Name</div>
+                    <div>Masseges</div>
+                </div>
+                <img src={logo} alt="Logo" className='h-20 w-20 my-auto border' />
+            </div>
+        </div>
+    )
+}
+
+export default Notification
